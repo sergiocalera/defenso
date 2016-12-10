c05e0801cfe7f545d389e59deccfa4fe715a72da
sergio calera <sergiocalera@gmail.com>
2016-12-09 23:05:28 -0600

    Se acompleto el trabajo de la pagina secciones

--- resources/views/viewIndex/secciones.blade.php
+++ resources/views/viewIndex/secciones.blade.php
@@ -7,9 +7,51 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+		<div class="hidden-md hidden-lg col-xs-12 col-sm-12 ">
 			<div class="row">
-				<a href="#">
+				<a href="{{ url('/defensora') }}">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="marco-derecho">
+							<div class="texto">
+								<table>
+									<tr>
+										<td style="vertical-align: middle;">
+											<p class="text-center">¿Quién es la Defensora?</p>
+										</td>
+									</tr>
+								</table>
+							</div>
+							<div class="icono-aux">
+								<img src="{{ url( 'img/index/Ic_1.png' ) }}" alt="icono-seccion">
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="row">
+				<a href="{{ url('/defensoria') }}">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="marco-derecho">
+							<div class="texto">
+								<table>
+									<tr>
+										<td style="vertical-align: middle;">
+											<p class="text-center">¿Qué es la Defensoría de las Audiencias?</p>
+										</td>
+									</tr>
+								</table>
+							</div>
+							<div class="icono-aux">
+								<img src="{{ url( 'img/index/Ic_1.png' ) }}" alt="icono-seccion">
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+		<div class="hidden-xs hidden-sm col-md-6 col-lg-6">
+			<div class="row">
+				<a href="{{ url('/defensora') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div id="primer-marco" class="marco-izquierdo">
 							<table>
@@ -22,7 +64,7 @@
 						</div>
 					</div>
 				</a>
-				<a href="">
+				<a href="{{ url('/defensoria') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div id="segundo-marco" class="marco-izquierdo">
 							<table>
@@ -39,7 +81,7 @@
 		</div>
 		<div id="col-derecha" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 			<div class="row">
-				<a href="#">
+				<a href="{{ url('/derechos') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="marco-derecho">
 							<div class="texto">
@@ -59,7 +101,7 @@
 				</a>
 			</div>
 			<div class="row">
-				<a href="#">
+				<a href="{{ url('/formulario') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="marco-derecho">
 							<div class="texto">
@@ -79,7 +121,7 @@
 				</a>
 			</div>
 			<div class="row">
-				<a href="#">
+				<a href="{{ url('/interes') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="marco-derecho">
 							<div class="texto">
@@ -99,7 +141,7 @@
 				</a>
 			</div>
 			<div class="row">
-				<a href="#">
+				<a href="{{ url('/normatividad') }}">
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 						<div class="marco-derecho">
 							<div class="texto">
