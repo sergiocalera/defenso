ac606a40eccb2850c404dca4b23c8102c9fb548d
Sergio Calera Fortis <SCALERA@tveducativa.mx>
2017-01-19 13:43:42 -0600

    Modificación a la plantilla del correo de respuesta

--- app/Correo/correo.php
+++ app/Correo/correo.php
@@ -22,7 +22,7 @@ class Correo{
 		$user = ['email_to' => $email_to, 'name_to' => $name_to, 'name_from' => $name_from, 'email_from' => $email_from];
 	
 		Mail::send(['html'=>'viewCorreo.response'], $data, function( $message ) use ($user){
-			$message->to( $user['email_to'], $user['name_to'] )->subject('Contestación a tu comentario');
+			$message->to( $user['email_to'], $user['name_to'] )->subject('Contestación a su comentario');
 			$message->from( $user['email_from'], $user['name_from'] );
 		});
 	}
--- resources/views/viewCorreo/response.blade.php
+++ resources/views/viewCorreo/response.blade.php
@@ -1,8 +1,15 @@
-Hola {{$name_to}}.<br>
-Tú mensaje a sido contestado con el siguiente correo electr&oacute;nico: {{ $email_to }} .<br>
+<p>
+	Estimado (a) {{$name_to}}.<br>
+	Su mensaje ha sido contestado al siguiente correo electr&oacute;nico: {{ $email_to }} .<br>
+</p>
 <h3>Respuesta</h3>
-{{ $response }}
-<br><br>
-Puedes revisarlo en el siguiente enlace <a href="{{ url( '/que-dice-audiencia' ) }}" target="_new">aquí.</a><br><br>
-Si tienes problemas con el link proporcionado, copia y pega el siguiente enlace.<br><br>
-{{ url( '/que-dice-audiencia' ) }}
+<p>
+	{{ $response }}
+</p>
+<br>
+<p>
+	Usted puede visualizar su mensaje y respuesta <a href="{{ url( '/que-dice-audiencia' ) }}" target="_new">aquí.</a>
+	<br>
+	Si tiene problemas con el enlace, copie y pegue la siguiente liga:<br><br>
+	{{ url( '/que-dice-audiencia' ) }}
+</p>
